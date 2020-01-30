e0439416d67f619ede698ed8f736a352d6fe1dbd
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2020-01-30 09:08:27 -0500

    Train App completed

--- assets/javascript/source.js
+++ assets/javascript/source.js
@@ -17,20 +17,23 @@ firebaseConfig = {
   //Write function to add new trains to the firebase database
 
 
+
+
 $("#add-train-btn").on("click", function(event){
     event.preventDefault();
-
+    newTrain();
+    function newTrain(traName, traDestination, traFrequency, traNext, traFirst) {
 
     var traName = $("#TrainName").val().trim();
     var traDestination = $("#Destination").val().trim();
     var traFrequency = $("#Frequency").val().trim();
     var traFirst = $("#First").val().trim();
-    var traNext = moment($("#Next").val().trim()).startof(traFrequency, 'mins').fromNow(traFirst); 
+    var traNext =  $("#Frequency").val().trim();
 
-    console.log(traName)
-    console.log(traDestination)
-    console.log(traFrequency)
-    console.log(traFirst)
+    // console.log(traName)
+    // console.log(traDestination)
+    // console.log(traFrequency)
+    // console.log(traFirst)
     
     var Train = {
         Name: traName,
@@ -44,11 +47,11 @@ $("#add-train-btn").on("click", function(event){
 
 
      
-     console.log(Train.Name);
-     console.log(Train.Destination);
-     console.log(Train.Frequency);
-     console.log(Train.First);
-     console.log(Train.Next);
+    //  console.log(Train.Name);
+    //  console.log(Train.Destination);
+    //  console.log(Train.Frequency);
+    //  console.log(Train.First);
+    //  console.log(Train.Next);
 
      alert("Train successfully added");
       
@@ -57,8 +60,8 @@ $("#add-train-btn").on("click", function(event){
       $("#Destination").val("");
       $("#Frequency").val("");
       $("#First").val("");
-      $("#Next").val("");
-});
+    }
+})
   // Write a function to pull info from firebase 
   
 
@@ -72,21 +75,88 @@ $("#add-train-btn").on("click", function(event){
     var traNext = childSnapshot.val().Next;
 
     //Train Details
-    console.log(traName);
-    console.log(traDestination);
-    console.log(traFrequency);
-    console.log(traFirst);
-    console.log(traNext);
+    // console.log(traName);
+    // console.log(traDestination);
+    // console.log(traFrequency);
+    // console.log(traFirst);
+    // console.log(traNext);
+  
     
-
     //create and display it in the table
+  //Next train arrival
+ //moment times
+ 
+   let now = new Date(moment());
+   let beginToday = new Date(moment().startOf("day"));
+
+   let current = (now - beginToday);
+   let f = JSON.stringify(traFirst);
+   let hour = f.slice(1, 3);
+   let min = f.slice(4, 6);
+   let theFirst = new Date(moment().startOf("day").add(hour, "hours").add(min, "minutes"));
+   let b = (theFirst - beginToday);
+   let between = (current - b);
+
+
+   var num = (((((current - b) / 1000) / 60) / traFrequency));
+   var numOfTrains = Math.abs(Math.floor(num));
+ 
+//  var next = (between + Math.floor((numOfTrains*60)));
+// //  var o =(Math.round(Math.abs(Math.round((next/1000)/60))));
+// //  console.log("n",o);
+
+   function minConvert(num) {
+     if (num <= 0) {
+       return 0
+     } else {
+       return 1
+     }
+   }
+
+
+   let final = ((minConvert(between) * numOfTrains) * traFrequency);
+
+   let next = (between + Math.floor(((numOfTrains * final) * 60)));
+
+   let o = (Math.round(Math.abs(Math.ceil((next / 1000) / 60))));
+   let nextTrain = Math.abs((o - final) - traFrequency);
+
+//  function time_convert(num)
+//  { 
+//   var hours = Math.floor(num / 60);  
+//   var minutes = num % 60;
+//   return hours + ":" + minutes;        
+// }
+
+// console.log(time_convert(next));
+// console.log(time_convert((nextTrain)));
+
+    
+    //main clock
+    function updateTime(){
+      const clock = document.getElementById("clock")
+      const now = moment();
+      const read = now.format("HH:mm");
+      clock.textContent =(read);
+      
+    }
+setInterval(updateTime,1000);
+updateTime();
+
+
  var tableRow = $("<tr>").append(
   $("<td>"). text(traName),
   $("<td>"). text(traDestination),
   $("<td>"). text(traFrequency),
-  $("<td>"). text(traFrequency),
-  // $("<td>"). text(),
+  $("<td>"). text(traFirst),
+  $("<td>"). text(nextTrain + " mins")
+  // .append(
+  //   $("<div>").append(
+  //     $("<p>").addClass(traName +"Counter").text(count + " mins")
+  //     ),
+  //   ),
 );
+
 
 $("#train-table > tbody").append(tableRow)
 });
